a18badb315d3276b6f7dc408051f5f09e7f5d335
Amogh Kashyap <94269573+amoghkash@users.noreply.github.com>
2025-10-13 18:56:56 -0400

    Add cases for motor control

--- main.py
+++ main.py
@@ -4,45 +4,57 @@ from enum import Enum
 import time
 
 class ControlType(Enum):
-        FORWARD = 1
-        BACKWORD = 2
-        STOP = 3
-        LEFT = 4
-        RIGHT = 5
-        STRAIGHT = 6
+    FORWARD = 1
+    BACKWARD = 2
+    STOP = 3
+    LEFT = 4
+    RIGHT = 5
+    STRAIGHT = 6
 
 def motor_control(q:mp.Queue):
-	while True:
-                try:
-                        if(not q.empty()):
-                                item = q.get()
-                                print(f"Got item {item}")
-                except:
-                        print("Exiting Process")
-                        break
+    while True:
+        try:
+            item = None
+            if(not q.empty()):
+                item = q.get()
+            else:
+                pass
+
+            match item[0]:
+                case ControlType.FORWARD:
+                    motor.motor_forward(int(item[1]))
+                case ControlType.BACKWARD:
+                    motor.motor_reverse(int(item[1]))
+                case ControlType.STOP:
+                    motor.motor_brake()
+
+        except:
+            print("Exiting Process")
+            break
 
 def input_control(q:mp.Queue):
-        counter = 0
-        while True:
-                try:     
-                        q.put([ControlType.FORWARD, counter])
-                        time.sleep(1)
-                except:
-                        print("Exiting Input")
-                        break
+    counter = 0
+    while True:
+        try:     
+            q.put([ControlType.FORWARD, counter % 100])
+            time.sleep(0.5)
+            counter += 1
+        except:
+            print("Exiting Input")
+            break
 
 if __name__ == "__main__":
 	# Setup Queue
-        # Input is [ControlType, Value]
-	q = mp.Queue()
+    # Input is [ControlType, Value]
+    q = mp.Queue()
 
 	# Setup Motor and Input Processes
-	motor_process = mp.Process(target=motor_control, args=(q,))
-	input_process = mp.Process(target=input_control, args=(q,))
+    motor_process = mp.Process(target=motor_control, args=(q,))
+    input_process = mp.Process(target=input_control, args=(q,))
 
-        # Start Processes
-	motor_process.start()
-	input_process.start()
+    # Start Processes
+    motor_process.start()
+    input_process.start()
 
-	motor_process.join()
-	input_process.join()
+    motor_process.join()
+    input_process.join()
