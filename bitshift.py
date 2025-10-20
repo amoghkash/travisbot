dd2744bb8ea41997f91fabc2f1a701b8dd4201da
TravisBot <94269573+amoghkash@users.noreply.github.com>
2025-10-19 22:07:16 -0400

    added mp3 file changes to make printing hex values more robust as well as necessary commands to initialize mp3 player correctly

---
+++ bitshift.py
@@ -0,0 +1,6 @@
+param: bytes= 0xFFFF
+param_shifted = param >>8
+print(param_shifted)
+print(type(param_shifted))
+print(param)
+print(type(param))
--- mp3test.py
+++ mp3test.py
@@ -1,6 +1,6 @@
 import serial
 from time import sleep
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
 sleep(4)
 def df_command(command, param=0, feedback=True):
     """
@@ -14,10 +14,10 @@ def df_command(command, param=0, feedback=True):
     Returns:
         bytes: The complete 10-byte command frame ready to send.
     """
-    start_byte = 0x7E
-    version = 0xFF
+    start_byte = 0x7e
+    version = 0xff
     length = 0x06
-    end_byte = 0xEF
+    end_byte = 0xef
     fb = 0x01
 
     param_high = (param >> 8) & 0xFF
@@ -29,32 +29,36 @@ def df_command(command, param=0, feedback=True):
     checksum_low = checksum & 0xFF
 
     # Construct full command frame
-    frame = bytes([
-        start_byte,
-        version,
-        length,
-        command,
-        fb,
-        param_high,
-        param_low,
-        checksum_high,
-        checksum_low,
-        end_byte
-    ])
+    frame = bytes([start_byte, version, length,
+                   command, fb, param_high, param_low,
+                   checksum_high, checksum_low, end_byte])
+    # 10/19/2025: First command: cmd: 0c params: 00 00
+    # 10/19/2025: 2nd command: cmd: 06 params: 00 0f
+    # 10/19/2025: third command: cmd: 07 params: 00 00  
+    # 10/19/2025: fourth command: cmd: 09 params: 00 01
+    
+    print(frame)
     return frame
-cmd = df_command(0x06, 25)
+media = 1 #what should the media equal? Answer: should equal 1
+cmd = df_command(0x09, media) 
+ser.write(cmd)
+cmd = df_command(0x06, 0x0F)
 #print(cmd)
 ser.write(cmd)
 
 cmd = df_command(0x0F, 2)  # 0x0F = "play track N"
 ser.write(cmd)
-ser.write(df_command(0x0F, 1, feedback=True))
+ser.write(df_command(0x0F, 2, feedback=True))
 ser.write(df_command(0x43))
+while True:
+    ser.write(b'\xff')
+    sleep(0.01)
+
 print(ser.read_all())
 try:
     while True:
         # Send "query current track" command with feedback
-        ser.write(df_command(0x43, feedback=True))
+        #ser.write(df_command(0x43, feedback=True))
 
         # Wait a short time for response
         sleep(0.1)
