75256f3e315dbccca36c98a0850e7df3c3341178
Amogh Kashyap <94269573+amoghkash@users.noreply.github.com>
2025-10-23 22:45:03 -0400

    add device input loop

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ if __name__ == "__main__":
     while True:
         try:
             item = None
+            device = evdev.InputDevice('/dev/input/event4') # Replace eventX with the correct device path
             for event in device.read_loop():
                 if event.type == evdev.ecodes.EV_KEY:
                     print(evdev.categorize(event))
